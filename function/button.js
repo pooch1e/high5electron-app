8c850f23456996a7e7d0f856fc900babde6ed471
Joel Kram <joelnskra@gmail.com>
2025-06-01 21:18:38 +0100

    encapsed hi5 function and fixed image styles

--- function/button.js
+++ function/button.js
@@ -1,21 +1,13 @@
 // hi-5 counter
-
-let count = 0;
-//add event listener to button
-const button = document.querySelector('button');
-const counter = document.querySelector('#counter');
-// interact on button click
-button.addEventListener('click', () => {
-  countIncrease();
-  renderCount();
-});
-
-function countIncrease() {
-  count++;
-}
-
-
-function renderCount() {
-  counter.innerHTML = count;
-  return counter;
-}
+const counterButton = () => {
+  let count = 0;
+  //add event listener to button
+  const button = document.querySelector('button');
+  const counter = document.querySelector('#counter');
+  // interact on button click
+  button.addEventListener('click', () => {
+    count++;
+    counter.innerHTML = count;
+  });
+};
+counterButton();
